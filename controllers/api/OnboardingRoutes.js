ade34ab29841a4913aba053c26db901115da7cbb
Shon Hatcher <shonhatcher@hotmail.com>
2021-03-24 21:14:12 -0400

    Updates to route

--- controllers/api/OnboardingRoutes.js
+++ controllers/api/OnboardingRoutes.js
@@ -2,62 +2,36 @@
 // const { Project } = require('../../models');
 // const withAuth = require('../../utils/auth');
 
+// route to create/add a onboarding plan
 router.post('/', async (req, res) => {
+    try { 
+      const onboardingData = await Onboarding.create({
+      name: req.body.name,
+      description: req.body.description,
+    });
+    res.status(200).json(onboardingData)
+  } catch (err) {
+    res.status(400).json(err);
+  }
+  });
+
+
+  router.put('/:id', async (req, res) => {
     try {
-      const onboardingData = await Onboarding.create(req.body);
-  
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
-  
-        res.status(200).json(userData);
-      });
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
-  
-  router.post('/login', async (req, res) => {
-    try {
-      const userData = await User.findOne({ where: { email: req.body.email } });
-  
-      if (!userData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
+      const onboardingData = await Onboarding.update(
+      {
+        name: req.body.name,
+        description: req.body.description,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
       }
-  
-      const validPassword = await userData.checkPassword(req.body.password);
-  
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-  
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
-        
-        res.json({ user: userData, message: 'You are now logged in!' });
-      });
-  
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
-  
-  router.post('/logout', (req, res) => {
-    if (req.session.logged_in) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
-  });
+   });
+
+//router.delete
+//router.get
   
   module.exports = router;
   

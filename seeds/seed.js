e979798ef594e2dc70825989914e0b9b4f8155cf
Shon Hatcher <shonhatcher@hotmail.com>
2021-03-29 19:46:38 -0400

    added Seed info

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,14 +1,18 @@
-const User = require('./User');
-const Roles = require('./Roles');
-
-
-Roles.hasOne(User, {
-  foreignKey: 'role_id',
-});
-
-User.belongsTo(Roles, {
-  foreignKey: 'role_id'
-});
-
-
-module.exports = { User,Roles };
+const sequelize = require('../config/connection');
+const { User, Roles } = require('../models');
+const userData = require('./userData.json');
+const roleData = require('./roleData.json');
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+  for (const role of roleData) {
+    await Roles.create({
+      ...role
+    });
+  }
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  process.exit();
+};
+seedDatabase();

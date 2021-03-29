b8c4318b93982da24508a2c76cfdb3680517444e
Faris Kadir <faris.kadir@gmail.com>
2021-03-29 19:18:09 -0400

    Created homeroute for employee, managers, and admin

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,50 +4,18 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
-
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
+    res.render('login', { 
+           logged_in: req.session.logged_in,
+           role: true
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/project/:id', async (req, res) => {
-  try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
 
-    const project = projectData.get({ plain: true });
 
-    res.render('project', {
-      ...project,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
@@ -59,10 +27,26 @@ router.get('/profile', withAuth, async (req, res) => {
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
+    switch (userData.role_id)   {
+        case 1:
+            res.render('admin', {
+                ...user,
+                logged_in: true
+            })
+            break;
+        case 2:
+            res.render('manager', {
+                ...user,
+                logged_in: true
+            })
+            break;
+        case 3:
+            res.render('employee', {
+                ...user,
+                logged_in: true
+            })
+            break;
+    }
   } catch (err) {
     res.status(500).json(err);
   }
@@ -71,7 +55,7 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/admin');
     return;
   }
 

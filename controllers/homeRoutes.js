b22e52c6b83169e025fe9a5e45c92d57149599b4
Faris Kadir <faris.kadir@gmail.com>
2021-03-30 20:34:26 -0400

    Updated the main page so that it only displays the nav bar if the user is logged in. Created an onboarding route to display onboarding documents.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -25,8 +25,6 @@ router.get('/profile', withAuth, async (req, res) => {
       attributes: { exclude: ['password'] }
     });
 
-    
-
     const user = userData.get({ plain: true });
 
     // Get all staff members (employees, managers, and admins)
@@ -101,5 +99,29 @@ router.get('/add', (req, res) => {
   }
   res.render('login');
 });
+
+
+// Use withAuth middleware to prevent access to route
+router.get('/onboarding', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] }
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('onboarding', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
 
 module.exports = router;

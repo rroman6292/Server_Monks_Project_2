ed45435a54beb5c162c65609e191056593273fc0
Faris Kadir <faris.kadir@gmail.com>
2021-03-30 21:55:04 -0400

    Finished the delete user route and created the delete.js file. Added the delete employee functionality to the manager page.

--- public/js/login.js
+++ public/js/login.js
@@ -22,33 +22,8 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const firstName = document.querySelector('#first-name-signup').value.trim();
-  const lastName = document.querySelector('#last-name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (firstName && lastName && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ firstName, lastName, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
 
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+
